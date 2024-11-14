0321fb4c16adb8239a1b4a47ce42cbadd93bf4cb
Louan Fontenele <louanbastos61@gmail.com>
2024-11-14 09:32:18 -0300

    Update index.tsx

--- src/components/generateKey/index.tsx
+++ src/components/generateKey/index.tsx
@@ -7,7 +7,7 @@ import { FiDownload } from "react-icons/fi";
 
 export default function GenerateKey() {
   const [registerName, setRegisterName] = useState("WinRAR");
-  const [licenseType, setLicenseType] = useState("Single PC");
+  const [licenseType, setLicenseType] = useState("Single-PC License");
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -22,7 +22,7 @@ export default function GenerateKey() {
     <div className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 flex items-center justify-center p-4">
       <div className="bg-white/10 backdrop-blur-lg rounded-xl p-8 w-full max-w-md shadow-xl">
         <div className="flex justify-between items-center mb-6 pb-4 border-b border-gray-700">
-          <h1 className="text-3xl font-bold text-white">Winrar Keygen</h1>
+          <h1 className="text-3xl font-bold text-white">WinRAR Keygen</h1>
         </div>
         <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
@@ -60,20 +60,20 @@ export default function GenerateKey() {
                 bg-[url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23FFFFFF%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E')]
                 bg-[length:12px_12px] bg-[right_1rem_center] bg-no-repeat"
             >
-              <option value="Single PC" className="bg-gray-800 text-white">
-                Single PC
+              <option value="Single-PC License" className="bg-gray-800 text-white">
+                Single-PC License
               </option>
-              <option value="Local Site" className="bg-gray-800 text-white">
-                Local Site
+              <option value="Corporate License" className="bg-gray-800 text-white">
+                Corporate License
               </option>
               <option
-                value="Unlimited Company"
+                value="Site License"
                 className="bg-gray-800 text-white"
               >
-                Unlimited Company
+                Site License
               </option>
-              <option value="1000 PC" className="bg-gray-800 text-white">
-                1000 PC
+              <option value="Unlimited Worldwide License" className="bg-gray-800 text-white">
+                Unlimited Worldwide License
               </option>
             </select>
           </div>
